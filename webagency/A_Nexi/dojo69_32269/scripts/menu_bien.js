96c731c52db373c9a29ab8dafcd9ad19ac67533b
G. Maingourd <guillaume.maingourd@ac3-groupe.com>
2019-12-10 12:19:41 +0100

    Auto commit A_Nexi/dojo69_32269

---
+++ webagency/A_Nexi/dojo69_32269/scripts/menu_bien.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('.mn-lvl2').children('li').each(function() {
+		if(!$(this).hasClass('.imageBien')) {
+			$(this).mouseover(function() {
+				var $class = $(this).attr('class').split('bien');
+				$(this).parent().children('.imageBien').html('<img src="$$_$$images/photo_biens/idbien'+$class[1]+'.jpg" />');
+			});
+			$(this).mouseout(function() {
+				$(this).parent().children('.imageBien').html('<img src="$$_$$images/photo_biens/idbien1.jpg" />');
+			});
+		}
+	});
+});
