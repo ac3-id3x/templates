37a344ab8832a057d225d4397241eceb35d7fea9
G. Maingourd <guillaume.maingourd@ac3-groupe.com>
2019-12-10 11:27:00 +0100

    Auto commit A_Nexi/boixadera11432_29708

---
+++ webagency/A_Nexi/boixadera11432_29708/scripts/menu.js
@@ -0,0 +1,42 @@
+var delai = 400;
+var timeout;
+var opened;
+/**
+ * rollover sur un bouton
+ */
+
+function rollover(n) {
+	//arreter timer
+	if(timeout) {
+		clearTimeout(timeout);
+		timeout = 0;
+		closeMenu()
+	}
+	//ouvrir le menu
+	document.getElementById("sm"+n).style.display = "block";
+	document.getElementById("mn"+n).style.backgroundPosition = "0 -53px";
+	document.getElementById("mn"+n).style.color = "#fff";
+	opened = n;
+}
+
+/**
+ * fermer le menu
+ */
+function closeMenu() {
+	document.getElementById("sm"+opened).style.display = "none";
+	document.getElementById("mn"+opened).style.backgroundPosition = "0 0";
+	document.getElementById("mn"+opened).style.color = "#fff";
+}
+
+//rollover sur un bouton, attendre avant de fermer le menu
+function rollout(n) {
+	closeDelay();
+}
+
+function closeDelay() {
+	if(timeout) {
+		clearTimeout(timeout);
+		timeout = 0;
+	}
+	timeout = setTimeout('closeMenu();',delai);
+}
