51ea2bea82ed6654287edd9a45bd722ca24435e5
G. Maingourd <guillaume.maingourd@ac3-groupe.com>
2019-12-10 11:24:38 +0100

    Auto commit A_Nexi/bi94_33066

---
+++ webagency/A_Nexi/bi94_33066/scripts/module/module_sofia_bandeau.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+	var bouton = 0;
+	var speed = 1000;
+	$('.flippingBandeau').each(function() {
+		$(this).css('cursor','pointer');
+		$(this).mouseover(function() {
+				$(this).addClass('borderFlipping');
+				$(this).children('.contentAnnonceBandeau').show();
+		});
+		$(this).mouseout(function() {
+				$(this).removeClass('borderFlipping');
+				$(this).children('.contentAnnonceBandeau').hide();
+		});
+	});
+	// FIXING SIZE
+	$('.moduleBandeau').each(function() {
+		$countDiv = $(this).find('.flippingBandeau').length;
+		if($countDiv <= 2) {
+			$(this).parent().parent().find('.prevBandeau').css('background','url()');
+			$(this).parent().parent().find('.nextBandeau').css('background','url()');
+		}
+		$flippingBandeauW = $(this).find('.flippingBandeau').outerWidth(true);
+		$calculW = $flippingBandeauW * $countDiv;
+		$(this).width($calculW);	
+	});
+	// NAV
+	$('.nextBandeau').css('cursor','pointer');
+	$('.nextBandeau').click(function() {
+		if(bouton == 0) {
+			// DETERMINE WHICH DIV SIZE
+			$countDiv = $(this).parent().find('.flippingBandeau').length;
+			$flippingBandeauW = $(this).parent().find('.flippingBandeau').outerWidth(true);
+			$calculW = $flippingBandeauW * $countDiv;
+			// END
+			bouton = 1;
+			var $position = $flippingBandeauW * 2;
+			var $container = $(this).parent().find('.moduleBandeau');
+			var $ml = parseInt($container.css('margin-left'));
+			var $calcul = $ml-$position;
+			
+			if(((($calcul-1) * -1)  == $calculW) || (($calcul * -1)  == $calculW)) {
+				$container.animate({marginLeft:0},speed,function(){bouton=0;});
+			} else {
+				if(($calcul * -1) >= $calculW) {
+					$calcul = 0;
+				}
+				$container.animate({marginLeft:$calcul},speed,function(){bouton=0;});
+			}
+		}
+	});
+	$('.prevBandeau').css('cursor','pointer');
+	$('.prevBandeau').click(function() {
+		if(bouton == 0) {
+			// DETERMINE WHICH DIV SIZE
+			$countDiv = $(this).parent().find('.flippingBandeau').length;
+			$flippingBandeauW = $(this).parent().find('.flippingBandeau').outerWidth(true);
+			$calculW = $flippingBandeauW * $countDiv;
+			// END
+			bouton = 1;
+			var $position = $flippingBandeauW * 2;
+			var $container = $(this).parent().find('.moduleBandeau');
+			var $ml = parseInt($container.css('margin-left'));
+			if($ml == 0 || $ml == -1) {
+				var $calcul = $ml-$position;
+				$container.animate({marginLeft:-$calculW-$calcul},speed,function(){bouton=0;});
+			} else {
+				var $calcul = $ml-($position * -1);
+				if($calcul > 0) {
+					$calcul = 0;
+				}
+				$container.animate({marginLeft:$calcul},speed, function(){bouton=0;});
+			}
+		}
+	});
+});
