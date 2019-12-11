188700814227538b21dc51b0eb7cf038f5d4bd90
G. Maingourd <guillaume.maingourd@ac3-groupe.com>
2019-12-11 16:43:12 +0100

    Auto commit A_Optiweb/Lancelot33

---
+++ webagency/A_Optiweb/Lancelot33/scripts/menu.js
@@ -0,0 +1,46 @@
+var delai = 400;
+var timeout;
+var opened;
+/**
+ * rollover sur un bouton
+ */
+
+function rollover(n) {
+	//arreter timer
+	if(timeout) {
+		clearTimeout(timeout);
+		timeout = 0;
+		closeMenu()
+	}
+	if(document.getElementById("sm"+n)) {
+		//ouvrir le menu
+		document.getElementById("sm"+n).style.display = "block";
+		document.getElementById("mn"+n).style.backgroundPosition = "0 -36px";
+		document.getElementById("mn"+n).style.color = "#fff";
+		opened = n;
+	}
+}
+
+/**
+ * fermer le menu
+ */
+function closeMenu() {
+	if(document.getElementById("sm"+opened)) {
+		document.getElementById("sm"+opened).style.display = "none";
+		document.getElementById("mn"+opened).style.backgroundPosition = "0 0";
+		document.getElementById("mn"+opened).style.color = "#fff";
+	}
+}
+
+//rollover sur un bouton, attendre avant de fermer le menu
+function rollout(n) {
+	closeDelay();
+}
+
+function closeDelay() {
+	if(timeout) {
+		clearTimeout(timeout);
+		timeout = 0;
+	}
+	timeout = setTimeout('closeMenu();',delai);
+}
