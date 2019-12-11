f845c970cbc9e6bbdd410fea2124c45f5fa3a023
G. Maingourd <guillaume.maingourd@ac3-groupe.com>
2019-12-11 16:09:12 +0100

    Auto commit A_Optiweb/aici-copie

---
+++ webagency/A_Optiweb/aici-copie/scripts/menu.js
@@ -0,0 +1,46 @@
+var delai = 700;
+var timeout;
+var opened;
+/**
+ * rollover sur un bouton
+ */
+
+function rollover(n) {
+	
+	//arreter timer
+	if(timeout) {
+		clearTimeout(timeout);
+		timeout = 0;
+		closeMenu()
+	}
+	//ouvrir le menu
+	if (document.getElementById("sm"+n)){
+		document.getElementById("sm"+n).style.display = "block";
+		document.getElementById("mn"+n).style.backgroundPosition = "0 -68px";
+	}
+	opened = n;
+	
+}
+
+/**
+ * fermer le menu
+ */
+function closeMenu() {
+	if (document.getElementById("sm"+opened)){
+	document.getElementById("sm"+opened).style.display = "none";
+	document.getElementById("mn"+opened).style.backgroundPosition = "0 0";
+}
+}
+
+//rollover sur un bouton, attendre avant de fermer le menu
+function rollout(n) {
+	closeDelay();
+}
+
+function closeDelay() {
+	if(timeout) {
+		clearTimeout(timeout);
+		timeout = 0;
+	}
+	timeout = setTimeout('closeMenu();',delai);
+}
