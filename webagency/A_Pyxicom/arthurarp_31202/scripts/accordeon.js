57f61fdc15b7efd4d6b6313c39ad20137b62a9a5
G. Maingourd <guillaume.maingourd@ac3-groupe.com>
2019-12-11 17:26:29 +0100

    Auto commit A_Pyxicom/arthurarp_31202

---
+++ webagency/A_Pyxicom/arthurarp_31202/scripts/accordeon.js
@@ -0,0 +1,17 @@
+﻿jQuery(document).ready(function(){
+
+		jQuery(".tab .blocAccord").hide();
+		jQuery(".tab h4").attr("class","tab_off");
+		
+		jQuery(".tab .blocAccord:first").show();
+		jQuery(".tab h4:first").attr("class","tab_on");
+		
+		jQuery(".tab h4").click(function(){
+			jQuery(this).parent().find(".blocAccord").slideUp(200);
+			jQuery(this).parent().find("h4").attr("class","tab_off");
+			jQuery(this).attr("class","tab_on");
+			jQuery(this).next(".blocAccord").slideDown();
+		});
+
+});
+	
---
+++ webagency/A_Pyxicom/arthurarp_31202/scripts/hauteur_bloc_gauche.js
@@ -0,0 +1,6 @@
+﻿jQuery(document).ready(function(){
+		var heightG= jQuery("#mid00").outerHeight();
+		heightG+=3;
+		jQuery("#fondHeader").animate({height:heightG+"px"},500);
+});
+	
