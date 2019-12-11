36e8f63279928681c714a5bf4ab6290f809d68ba
G. Maingourd <guillaume.maingourd@ac3-groupe.com>
2019-12-11 17:47:11 +0100

    Auto commit A_Pyxicom/Denard31

---
+++ webagency/A_Pyxicom/Denard31/scripts/clign.js
@@ -0,0 +1,14 @@
+
+function clignotement()
+{
+	if(document.getElementById("MonElement"))
+	{
+	    if (document.getElementById("MonElement").style.display=="block") 
+	       document.getElementById("MonElement").style.display="none"; 
+	    else 
+	       document.getElementById("MonElement").style.display="block";
+	}
+} 
+// mise en place de l appel régulier de la fonction toutes les 0.5 secondes 
+setInterval("clignotement()", 1000); 
+
