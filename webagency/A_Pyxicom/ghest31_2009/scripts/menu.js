f902756b571d51014b34d6739c4433df684e8fdb
G. Maingourd <guillaume.maingourd@ac3-groupe.com>
2019-12-11 17:55:36 +0100

    Auto commit A_Pyxicom/ghest31_2009

---
+++ webagency/A_Pyxicom/ghest31_2009/scripts/menu.js
@@ -0,0 +1,21 @@
+var obj = null;
+function checkHover() {
+	if (obj) {
+		obj.find('.smenu').fadeOut('fast');	
+	} //if
+} //checkHover
+
+jQuery(document).ready(function() {
+	jQuery('ul > li').hover(function() {
+		if (obj) {
+			obj.find('.smenu').slideUp('fast');
+			obj = null;
+		} //if
+		jQuery(this).find('.smenu').slideDown('fast');
+	}, function() {
+		obj = jQuery(this);
+		setTimeout(
+			"checkHover()",
+			0); // si vous souhaitez retarder la disparition, c'est ici
+	});
+});
