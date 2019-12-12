39cf9535e863e3d0930c2895d5c961da2ed1b472
G. Maingourd <guillaume.maingourd@ac3-groupe.com>
2019-12-12 09:17:19 +0100

    Auto commit A_Pyxicom/villennes_28335

---
+++ webagency/A_Pyxicom/villennes_28335/scripts/menu.js
@@ -0,0 +1,22 @@
+jQuery.noConflict();
+var obj = null;
+function checkHover() {
+	if (obj) {
+		obj.find('div').fadeOut('fast');	
+	} //if
+} //checkHover
+
+jQuery(document).ready(function() {
+	jQuery('ul > li').hover(function() {
+		if (obj) {
+			obj.find('div').slideUp('fast');
+			obj = null;
+		} //if
+		jQuery(this).find('div').slideDown('fast');
+	}, function() {
+		obj = jQuery(this);
+		setTimeout(
+			"checkHover()",
+			0); // si vous souhaitez retarder la disparition, c'est ici
+	});
+});
