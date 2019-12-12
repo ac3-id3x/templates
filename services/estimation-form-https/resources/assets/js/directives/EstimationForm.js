954cbb3b707cb9dde2809860d2d42f1ddc82dd42
G. Maingourd <guillaume.maingourd@ac3-groupe.com>
2019-12-12 14:52:46 +0100

    Suppression dossiers inutiles pour les sites agence

--- services/estimation-form-https/resources/assets/js/directives/EstimationForm.js
+++
@@ -1,8 +0,0 @@
-export default ['Config', (Config) => {
-    return {
-        restrict: 'E',
-        templateUrl(element, attrs) {
-            return `${Config.get('app.paths.views')}/partials/_layout-${attrs.layout || 'main'}.html`;
-        }
-    };
-}];
