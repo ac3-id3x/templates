d09baafb4660cdcfaf07be9cfd07bdc4943b1ca4
G. Maingourd <guillaume.maingourd@ac3-groupe.com>
2019-12-12 14:54:55 +0100

    Commit dossier slagence

---
+++ webagency/slagence/selection.js
@@ -0,0 +1,4 @@
+$$DYN:ID3:TOJS$$$$E:ID3:NOUACACHE$$
+$$E:IMMOBW:PANIER:NBANN$$
+$$IF:IMMOBW:PANIER:NBANN$$
+<A href="/recherche.htm">$$IMMOBW:PANIER:NBANN$$ annonce$$IMMOBW:PANIER:NBANN pluriel$$ dans votre panier</A>$$END$$
