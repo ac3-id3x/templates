a8b41000f7ca97656118dfef61ba0f7f7d69398d
G. Maingourd <guillaume.maingourd@ac3-groupe.com>
2019-12-13 09:43:37 +0100

    Auto commit A_Nexi/truche_35386_2

---
+++ webagency/A_Nexi/truche_35386_2/scripts/flickr.js
@@ -0,0 +1,470 @@
+// DESIGN
+$('#flickr-design1').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644059159785',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-design2').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643702651795',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-design3').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643703159953',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-design4').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643702300935',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-design5').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643702737823',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-design6').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644753052927',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 1, borderWeight: 6});
+
+$('#flickr-design7').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157655788513810',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 1, borderWeight: 6});
+
+
+$('#flickr-design8').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157662684715000',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 1, borderWeight: 6});
+
+
+
+// TRADITIONNELLES
+$('#flickr-trad1').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157672759009602',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad2').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644059684503',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad3').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644059437824',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad4').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644059392624',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad5').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644059198205',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad6').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644058586414',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad7').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644051992755',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad8').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644006671854',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad9').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643703415483',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad10').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643702370164',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad11').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643702480983',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad12').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643701601113',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad13').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157640541468444',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-trad14').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157654943126280',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+
+//CONTEMPORAINES
+$('#flickr-contemp1').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644058777994',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-contemp2').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644058848663',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-contemp3').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644052291634',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-contemp4').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644052103324',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-contemp5').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644052064454',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-contemp6').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157644006408583',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-contemp7').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643702310413',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-contemp8').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643701078565',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-contemp9').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643699511153',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+$('#flickr-contemp10').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157656217658105',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+
+$('#flickr-contemp11').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157673014581040',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 1, borderWeight: 6});
+
+
+
+// DECO
+$('#flickr-deco1').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643319003514',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+
+$('#flickr-deco2').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643318807694',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+
+$('#flickr-deco3').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157643301477554',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+
+$('#flickr-deco4').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157660925858054',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
+
+
+$('#flickr-avt1').photocradle(
+   {
+      service: 'flickr',
+      photoset: '72157650519791661',
+      imageSizes:
+      {
+         thumbnail: $.photocradle.flickrImageSizes.small320,
+         preview: $.photocradle.flickrImageSizes.medium640,
+         original: $.photocradle.flickrImageSizes.large
+      }
+   }, {firstImageIndex: 4, borderWeight: 6});
